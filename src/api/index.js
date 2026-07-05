a617bc84bfa1f8e9519b0eb8ee46ddb11af8e4ff
srilaxmi <srilaxmivallampatla9@gmail.com>
2026-07-05 22:42:51 +0530

    Update backend API URL for production

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://issuetrackerbackend-x7ey.onrender.com/api' });
+const API = axios.create({ baseURL:process.env.REACT_APP_API_URL || 'http://localhost:8080/api' });
 
 // AUTH
 export const login = (data) => API.post('/login', data);
